b43338c7cf9bfb008da4d9617dc15564a72ca739
Wesley M Lawson <153121346+wesley-lawson13@users.noreply.github.com>
2026-07-03 21:28:18 -0400

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        pre, suf = [0] * len(height), [0] * len(height)
+        for i, num in enumerate(height):
+            if i == 0:
+                pre[i] = num
+                continue
+            
+            pre[i] = max(pre[i-1], num)
+
+        for i in range(len(height) - 1, -1, -1):
+            if i == len(height) - 1:
+                suf[i] = height[i]
+                continue
+
+            suf[i] = max(suf[i+1], height[i])
+
+        print(f"pre: {pre}, suf: {suf}")
+        total = 0
+        for i in range(len(height)):
+            
+            c = min(pre[i], suf[i])
+            total += max(0, c - height[i])
+
+        return total
+
+            
